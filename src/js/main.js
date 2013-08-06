1bbc86b224506516e736c02fbda74d677b5787a4
Andrew Betts <andrew@trib.tv>
2013-08-06 20:16:35 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 var c = new require('test-c');
-var int = new require('othermodule.js');
+var my = new require('othermodule.js');
 
 exports.run = function() {
 
-  return c.run() + int.run();
+  return c.run() + my.run() + "[B:v1]";
 
 };
